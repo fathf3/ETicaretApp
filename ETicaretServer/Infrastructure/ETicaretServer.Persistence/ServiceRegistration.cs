56103c5ee6a4e32828bf43337752e96ff7210ff7
fathf3 <fatih-348@hotmail.com>
2024-10-28 16:47:11 +0300

    Add your commit

---
+++ ETicaretServer/Infrastructure/ETicaretServer.Persistence/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretServer.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETicaretServer.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETicaretAPIDbContext>(options =>
+            {
+                options
+                .UseNpgsql(configuration.GetConnectionString("PostgreSQL"));
+            });
+        }
+    }
+}
---
+++ ETicaretServer/Presentation/ETicaretServer.API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //private readonly IProductService _productService;
+
+        //public ProductsController(IProductService productService)
+        //{
+        //    _productService = productService;
+        //}
+
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    return Ok(_productService.GetProducts());
+        //}
+    }
+}
